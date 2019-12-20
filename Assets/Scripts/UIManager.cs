50ed37809d07fc47961075f5060884257f2456d0
Simone Starace <simone.starace93@gmail.com>
2019-12-20 12:22:51 +0100

    Added the UIManager C# Script

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using TMPro;
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI _storyText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(_storyText)
+        {
+            _storyText.text = "Hello World!";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
